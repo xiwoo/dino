00ea6324332fa1fc3b35aa5a713016a4ed18d43a
xiwooDesktop <xiwoo012@gmail.com>
2021-09-10 17:01:19 +0900

    square dino update

--- main.js
+++ main.js
@@ -20,25 +20,62 @@ class Cactus {
 
 const dino = new Cactus(10, 200, 50, 50, 'green');
 
+let animation;
 let timer = 0;
 const cactuses = [];
+let jumpTimer = 0;
+let jump = false;
 
 function frameFunc() {
-  requestAnimationFrame(frameFunc);
+  animation = requestAnimationFrame(frameFunc);
   timer++;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if(timer % 120 === 0) {
+  if(timer % 200 === 0) {
     const cactus = new Cactus(canvas.width, 200, 50, 50, 'red');
     cactuses.push(cactus);
     cactus.draw();
   }
   
-  cactuses.forEach(x => {
+  cactuses.forEach((x, i, o) => {
+    if(x.x < 0) {
+      o.splice(i, 1)
+    }
     x.x--;
+    crashCheck(dino, x);
     x.draw();
-  })
+  });
+
+  
+  if(jump) {
+    dino.y--;
+    jumpTimer++;
+  }
+  else if(dino.y < 200){
+    dino.y++;
+  }
+  if(jumpTimer > 100) {
+    jump = false;
+    jumpTimer = 0;
+  }
   dino.draw();
 }
 
+
 frameFunc();
+
+function crashCheck(dino, cactus) {
+  const xCalc = cactus.x - (dino.x + dino.width);
+  const yCalc = cactus.y - (dino.y + dino.height);
+  if(xCalc < 0 && yCalc < 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(animation);
+  }
+}
+
+
+document.addEventListener('keydown', e => {
+  if(e.code === 'Space') {
+    jump = true;
+  }
+})
